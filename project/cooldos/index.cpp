5faeab2f345486c567c7371adfa04bad6c84b352
eliacademia <elijahwongso07@gmail.com>
2026-01-22 12:46:32 -0300

    first commit

--- project/cooldos/index.cpp
+++ project/cooldos/index.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    cout<< "Hello World" << endl;
+    //This is the welcome page that asks if you are an admin or guest and asks for your pw
+    
+    string userAnswer;
+    string password; // Guest password: guest.pw   Admin password: admin.pw
+
+     cout << "Welcome to Cooldos Motors! Are you a guest or an admin?" << endl;
+    cin >> userAnswer;
+
+    cout << "Please enter your password: " << endl;
+    cin >> password;
+
+    if (password== "guest.pw" || "admin.pw"){
+
+        cout << "Please enter what you would like to view: " << endl;
+    }
+
+    else{
+        cout << "Please enter your correct password: " << endl;
+    }
+
+    string viewOptions[]= { "Available cars", "Purchase history", "Wishlist"};
+
+    for(int i=0; i<3; i++){
+        cout << viewOptions[i] << endl;
+    }
+    
     return 0;
 }
