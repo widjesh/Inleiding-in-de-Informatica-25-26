2876ea82c3a02076eda07c54b5d27939eb9fd86f
sheldondaim <sheldonsetroredjo@gmail.com>
2026-01-27 14:23:39 -0300

    Add user input for greeting in index.cpp

--- project/terminator/index.cpp
+++ project/terminator/index.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
+	 string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "! Welcome to ADEK." << endl;
     cout<< "bleh bleh bleh" << endl;
 	cout << "hello allemaal" << endl;
 
