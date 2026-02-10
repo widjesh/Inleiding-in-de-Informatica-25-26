7b54c11c1fa67558b03c15db14a83d9e08b3fca2
lebroooooooon <yushuasalim@gmail.com>
2026-02-10 14:31:42 -0300

    hmm

--- project/terminator/index.cpp
+++ project/terminator/index.cpp
@@ -6,7 +6,6 @@ int main(){
     cout << "Enter your name: ";
     cin >> name;
     cout << "Hello, " << name << "! Welcome to ADEK." << endl;
-    cout<< "bleh bleh bleh" << endl;
 	cout << "hello allemaal" << endl;
 
     return 0;
