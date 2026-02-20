800b658d7377119dc699792bbe30ec026facf7eb
amarisgx <amarisfgobind@gmail.com>
2026-02-19 22:29:34 -0300

    firstOption = 1

--- project/masala/index.cpp
+++ project/masala/index.cpp
@@ -31,3 +31,17 @@ int birdLookupAll() {
     return x;
 }
 
+  if (firstOption == 1) {
+        cout << "Welcome, do you want to view the whole database or search a specific bird?" << endl;
+        cout << "[A] for whole database" << endl;
+        cout << "[ID] for specific bird" << endl;
+        cin >> birdIDOrAll;
+
+        if (birdIDOrAll == "A") {
+            cout << birdLookupAll() << endl;
+        } else if(birdIDOrAll == "ID") {
+            cout << birdLookupSpecific() << endl;
+        } else {
+            cout << "Please put in A or ID(Case sensitive)" << endl;
+            cin >> birdIDOrAll;
+        };
