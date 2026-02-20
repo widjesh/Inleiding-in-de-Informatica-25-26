1012873758ce993e401de7de52ccceed8c766241
ishoediah <oeditishaan@gmail.com>
2026-02-19 21:37:24 -0300

    Upload Code Class and Functions

---
+++ project/masala/index.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Bird {
+    public:
+        int ID; 
+        string birdName;
+        string birdType;
+        string birdSpecies;
+        string scientificName;
+        string typeOfInjury;
+        string dateIn;
+        string dateOut;
+
+};
+
+int createNewBird() {
+
+}
+
+int birdLookupSpecific( ) {
+    int test = 5;
+    return test;
+};
+
+int birdLookupAll() {
+    int x = 6;
+    return x;
+}
+
