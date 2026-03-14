4237116c0d3ac461e1fb09bc1540785d2785fc70
salchetal <84756418+salchetal@users.noreply.github.com>
2026-03-14 15:27:26 -0300

    Implement Caesar cipher function
    Een Caesar cipher function toegevoegd, deze gaat gebruikt worden om de passwoorden en de usernames to "veranderen" (soort van low level hash, die reversable is), en deze worden dan opgeslagen in een txt file.

--- project/x/index.cpp
+++ project/x/index.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 using namespace std;
 
+string caesar_cipher(string word, int shift){
+    int i;              // loop counter used to go through the word
+    char currentChar;   // stores the current character from the word
+    int value;          // numerical ASCII value used for shifting
+
+    
+    for (i = 0; i < word.length(); i++)
+    {
+        currentChar = word[i];
+
+        
+        if (currentChar >= 'a' && currentChar <= 'z')
+        {
+            value = currentChar + shift;
+
+            while (value > 'z')
+                value = value - 26;
+
+            while (value < 'a')
+                value = value + 26;
+
+            word[i] = value;
+        }
+
+        
+        else if (currentChar >= 'A' && currentChar <= 'Z')
+        {
+            value = currentChar + shift;
+
+            while (value > 'Z')
+                value = value - 26;
+
+            while (value < 'A')
+                value = value + 26;
+
+            word[i] = value;
+        }
+
+       
+        else if (currentChar >= '0' && currentChar <= '9')
+        {
+            value = currentChar + shift;
+
+            while (value > '9')
+                value = value - 10;
+
+            while (value < '0')
+                value = value + 10;
+
+            word[i] = value;
+        }
+    }
+
+    return word;
+}
 int main(){
     cout<< "Hello World" << endl;
 }
